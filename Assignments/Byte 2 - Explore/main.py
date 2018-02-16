ce3dca4ff7c240bda46090380cea2d6205217863
Supawat Vitoorapakorn <svitoora@andrew.cmu.edu>
2018-02-15 22:09:08 -0500

    Alabamba is working

--- Assignments/Byte 2 - Explore/main.py
+++ Assignments/Byte 2 - Explore/main.py
@@ -33,10 +33,13 @@ service = build('fusiontables', 'v1', developerKey=API_KEY)
 # This is the table id for the fusion table
 TABLE_ID = '1RVTzMa7zv2xb8NM_Xt3Wvs-8KBhCY9-xJ4nNBNQ'
 
+
+
+
+
 # This is the default columns for the query
 query_cols = []
-query_values = [10]
-
+query_values = ['Alabama'] #Change to be the value(s) you're querying in the column you've specified
 
 # Import the Flask Framework
 from flask import Flask, request
@@ -72,7 +75,7 @@ def make_query(cols, values, limit):
     string_values = string_values[2:len(string_values)]
     
     #Change this query to have your corresponding column (in our soccer example, the column for our WHERE is Scorer).
-    query = "SELECT " + string_cols + " FROM " + TABLE_ID + " WHERE RANK < " + string_values
+    query = "SELECT " + string_cols + " FROM " + TABLE_ID + " WHERE STATE = '" + string_values + "'"
 
     query = query + " LIMIT " + str(limit)
 
@@ -88,7 +91,7 @@ def make_query(cols, values, limit):
 def index():
     template = JINJA_ENVIRONMENT.get_template('templates/index.html')
     request = service.column().list(tableId=TABLE_ID)
-    res = get_all_data(make_query([], query_values, 50)) #5 is our limit we're passing in
+    res = get_all_data(make_query([], query_values, 5)) #5 is our limit we're passing in
     logging.info('allheaders')
     return template.render(columns=res['columns'], rows = res['rows'] )
 
